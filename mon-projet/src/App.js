b79b27e85e0f780e3c8487cb7c383393246d3897
Vadim Rader <vadim.radermacher@gmail.com>
2020-01-06 15:43:21 +0100

    skeleton

--- mon-projet/src/App.js
+++ mon-projet/src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Dropdown } from 'react-bootstrap';
 import './App.css';
+import './Sass.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="bg-dark">
+      <div className="section">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-5">
+              <div className="text-light border-left border-light p-2 slideInLeft">
+                <h1>Vadim Radermacher</h1>
+                <small>Junior Web Developper</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section bg-light">
+        <div className="container">
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Dropdown Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        </div>
+      </div>
+    </section>
   );
 }
 
